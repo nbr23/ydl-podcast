a1654562699d65c4cc799ea59af3fe2d4725c039
Maxence <max@23.tf>
2024-06-04 22:22:29 -0400

    a bit of styling on the index

--- ydl_podcast/template.py
+++ ydl_podcast/template.py
@@ -49,6 +49,32 @@ INDEX_HTML_TMPL = """
 <html manifest="" lang="en-US">
   <head>
     <meta charset="UTF-8">
+    <style>
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 20px;
+}
+
+h1 {
+    margin-bottom: 20px;
+    font-size: 2em;
+    margin: 0;
+}
+
+ul {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+li {
+    display: flex;
+    overflow: hidden;
+    list-style-type: none;
+}
+    </style>
   </head>
   <body>
     <h1>Ydl-Podcast index</h1>
